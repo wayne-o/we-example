b79a4494c5917e7c2bd92d9d77b0ddf8e65891aa
Alberto Souza <alberto.souza.99@gmail.com>
2014-10-15 02:24:11 -0300

    fix isLoggedInPolicy authenticated user check

--- api/policies/isLoggedInPolicy.js
+++ api/policies/isLoggedInPolicy.js
@@ -8,7 +8,7 @@
  */
 
 module.exports = function(req, res, next) {
-  if( _.isEmpty(req.user) || _.isEmpty(req.user.id)){
+  if( !req.isAuthenticated() ){
     sails.log.verbose('User not authenticated ', req.options, req.user);
     return res.forbidden({
       error: 'You are not permitted to perform this action.'
