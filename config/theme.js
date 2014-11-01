dc8500b4511d67e252e9fae95ce92053b036bc1d
Alberto Souza <alberto.souza.99@gmail.com>
2014-11-01 17:00:58 -0200

    added admin theme config

--- config/theme.js
+++ config/theme.js
@@ -4,6 +4,7 @@
 
 module.exports.themes = {
   // theme enabled
-  enabled: 'wejs-theme-default'
-
+  enabled: 'wejs-theme-default',
+  // admin active theme for admin urls /admin*
+  admin: 'we-theme-admin-default'
 };
